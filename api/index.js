060a0b9c50fffc411f7756ee6a9f7f853f3d479d
ajcodeskills <martinaj9514@gmail.com>
2021-07-26 16:08:51 +0530

    user authentication api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 require("dotenv").config();
 const app = express();
 
@@ -8,14 +9,14 @@ mongoose
   .connect(process.env.MONGOURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
   .then((res) => console.log("Connected to database"))
   .catch((err) => console.log(err));
 
-
-app.use(express.json())
-  app.use("/api/auth", authRoute);
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT, () => {
   console.log(`listening to the port ${process.env.PORT}`);
